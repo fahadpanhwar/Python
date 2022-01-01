aa798f67bb49b0d25660d07302836fe33c549603
fahadpanhwar <fahadpanhwar713@gmai.com>
2022-01-01 08:48:15 -0800

    Work Complete

---
+++ Assignment1/hello.py
@@ -0,0 +1,72 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 28,
+   "id": "620f24b7",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "twinkle, twinkle, little star,\n",
+      "\thow i wonder what you are! \n",
+      "\t\tup above the world so high, \n",
+      "\t\tlike a diamond in the sky. \n",
+      "twinkle, twinkle, little star, \n",
+      "\thow i wonder what you are\n"
+     ]
+    }
+   ],
+   "source": [
+    "print(\"twinkle, twinkle, little star,\\n\\thow i wonder what you are! \\n\\t\\tup above the world so high, \\n\\t\\tlike a diamond in the sky. \\ntwinkle, twinkle, little star, \\n\\thow i wonder what you are\")"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "18c2c1ac",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "bd1e40af",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "b481fc24",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.7"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
